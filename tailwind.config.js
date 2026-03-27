c8355a3f03239f7c1f0216f5bac5b419d2e4a996
spracknow-droid <spracknow@gmail.com>
2026-03-27 11:25:48 +0900

    Update Tailwind CSS config to include pages and components
    Added monitoring for files in pages and components directories.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // app 폴더 안의 모든 파일을 감시
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
